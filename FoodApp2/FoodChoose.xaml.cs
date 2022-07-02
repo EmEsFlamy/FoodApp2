61e87ea46703c2fe3bf57b99da2965f6b3506c53
Rogal <Rogal@DESKTOP-Q58Q82T>
2022-07-02 21:20:15 +0200

    Add project files.

---
+++ FoodApp2/FoodChoose.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FoodApp2
+{
+    /// <summary>
+    /// Interaction logic for FoodChoose.xaml
+    /// </summary>
+    public partial class FoodChoose : Window
+    {
+        public FoodChoose()
+        {
+            InitializeComponent();
+            FoodAppEntities FA = new FoodAppEntities();
+
+
+
+
+            this.Usersgrid2.ItemsSource = FA.Users.ToList();
+            this.RestaurantGrid.ItemsSource = FA.Restaurant.ToList();
+            this.FoodTypeGrid.ItemsSource =FA.FoodType.ToList();
+            this.FoodGrid.ItemsSource = FA.Food.ToList();
+            
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            OrderGrid.Items.Add(new 
+                {
+                    Name = Name2TXT.Text,
+                    Surname = Surname2TXT.Text,
+                    Address = Address2TXT.Text,
+                    PhoneNumber = PhoneNumber2TXT.Text,
+                    RestaurantName = RestaurantTXT.Text,
+                    FoodTypeName = FoodTypeTXT.Text,
+                    FoodName = FoodTXT.Text
+                });
+                
+            
+        }
+    }
+}
---
+++ FoodApp2/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FoodApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            FoodAppEntities FA = new FoodAppEntities();
+            var users = from d in FA.Users
+                        select new
+                        {
+                            ClientName = d.Name,
+                            ClientSurname = d.Surname,
+                            ClientAddres = d.Address,
+                            ClientPhoneNumber = d.PhoneNumber
+                        };
+            
+            this.Gridusers.ItemsSource = users.ToList();
+        }
+
+        private void btnAdd_click(object sender, RoutedEventArgs e)
+        {
+            FoodAppEntities FA = new FoodAppEntities();
+
+            Users Userobject = new Users()
+            {
+                Name = txtName.Text,
+                Surname = txtSurname.Text,
+                Address = txtAddress.Text,
+                PhoneNumber = txtPhoneNumber.Text
+            };
+            FA.Users.Add(Userobject);
+            FA.SaveChanges();
+        }
+
+        private void btnLoadUsers_Click(object sender, RoutedEventArgs e)
+        {
+            FoodAppEntities FA = new FoodAppEntities();
+
+            this.Gridusers.ItemsSource = FA.Users.ToList();
+        }
+
+        private long upadatingUserID = 0;
+        private void Gridusers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FoodAppEntities FA = new FoodAppEntities();
+            if (this.Gridusers.SelectedIndex >= 0)
+            {
+                if (this.Gridusers.SelectedItems.Count >= 0)
+                {
+                    if (this.Gridusers.SelectedItems[0].GetType() == typeof(Users))
+                    {
+                        Users d = (Users)this.Gridusers.SelectedItems[0];
+                        this.txtName2.Text = d.Name;
+                        this.txtSurname2.Text = d.Surname;
+                        this.txtAddress2.Text = d.Address;
+                        this.txtPhoneNumber2.Text = d.PhoneNumber;
+                        this.upadatingUserID = d.userID;
+                        FA.SaveChanges();
+                    }
+
+                    
+                }
+            }
+
+
+        }
+
+        private void btnUpdateUser_Click(object sender, RoutedEventArgs e)
+        {
+            FoodAppEntities FA = new FoodAppEntities();
+
+            var r = from d in FA.Users
+                    where d.userID == this.upadatingUserID
+                    select d;
+
+            var user = FA.Users.FirstOrDefault(x => x.userID == ((Users)Gridusers.SelectedItem).userID);
+
+            if (user != null)
+            {
+                user.Name = this.txtName2.Text;
+                user.Surname = this.txtSurname2.Text;
+                user.Address = this.txtAddress2.Text;
+                user.PhoneNumber = this.txtPhoneNumber2.Text;
+                FA.SaveChanges();
+            }
+
+
+
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            FoodAppEntities FA = new FoodAppEntities();
+            
+            
+
+            
+                var user = FA.Users.FirstOrDefault(x => x.userID == ((Users)Gridusers.SelectedItem).userID);
+
+
+
+                if (user != null)
+                {
+
+                    FA.Users.Remove(user);
+                    FA.SaveChanges();
+                }
+
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FoodChoose win2 = new FoodChoose();
+            win2.Show();
+            this.Close();
+        }
+    }
+}
